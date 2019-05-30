4d49b557eaa796a077ea68e574de7cdd6f1ee5e0
huangkaili <huangkaili@idcmatic.com>
2019-05-30 18:44:48 +0800

    移动

---
+++ phone/phone/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// 在入口文件中全部导入
+// import Cube from 'cube-ui'
+
+// Vue.use(Cube)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
